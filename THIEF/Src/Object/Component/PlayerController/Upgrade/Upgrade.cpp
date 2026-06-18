d8bb833f0d26812734c25d3a2941e427580dfcf5
miiiiiira <ichigokanure@gmail.com>
2026-06-19 00:40:24 +0900

    ショップのフェードの種類変更

--- THIEF/Src/Object/Component/PlayerController/Upgrade/Upgrade.cpp
+++ THIEF/Src/Object/Component/PlayerController/Upgrade/Upgrade.cpp
@@ -498,7 +498,7 @@ void Upgrade::ConfirmUpgrade(void)
 		// アップグレードを終了させる
 		UpgradeManager::GetInstance().TrueIsUpgradeEnd();
 		// ゲームシーンへ
-		SceneManager::GetInstance()->NextChangeScene(std::make_shared<GameScene>());
+		SceneManager::GetInstance()->NextChangeScene(std::make_shared<GameScene>(), false, Fader::TYPE::SHUTTER);
 
 		return;
 	}
