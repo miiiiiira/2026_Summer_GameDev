9a7bc334654b8b8db608747cde2df018316e9661
Karin-518 <karin.410@ezweb.ne.jp>
2026-07-14 09:34:51 +0900

    Tutorial基盤追加しました

---
+++ THIEF/Src/Scene/Tutorial/TutorialInfo.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+
+struct TutorialInfo
+{
+	int id;
+	int type;
+	float target;
+	float  value;
+	std::string text;
+};
---
+++ THIEF/Src/Scene/Tutorial/TutorialScene.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "../SceneBase.h"
+
+class TutorialScene : public SceneBase
+{
+public:
+
+private:
+};
+
