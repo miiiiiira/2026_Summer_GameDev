92ceb0dab67e70b1cd1afc376b3458638756ffb9
miiiiiira <ichigokanure@gmail.com>
2026-08-10 16:45:20 +0900

    ステージ3に地図を追加

--- THIEF/Src/Object/Component/PlayerController/Map/MapInfo.cpp
+++ THIEF/Src/Object/Component/PlayerController/Map/MapInfo.cpp
@@ -6,6 +6,6 @@ namespace MapTable
 	{
 		{ STAGE_NUM::STAGE_1,	{"Data/Image/GameScene/Map.png"	,843,1024,610,982}},
 		{ STAGE_NUM::STAGE_2,	{"Data/Image/GameScene/Map2.png" ,938,719,868,599 }},
-		{ STAGE_NUM::STAGE_3,	{"Data/Image/GameScene/Map.png" ,843,1024,610,982} },
+		{ STAGE_NUM::STAGE_3,	{"Data/Image/GameScene/Map3.png" ,905,906,130,811} },
 	};
 }
